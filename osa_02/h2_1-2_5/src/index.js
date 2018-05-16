67a44393199a7e23809fe1e9ef85fd94a143b045
durak <itapkos@gmail.com>
2018-05-16 22:19:22 +0300

    h2_1

---
+++ osa_02/h2_1-2_5/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Osa = ({nimi, tehtavia}) => <p>{nimi} {tehtavia}</p>
+
+const Otsikko = ({text}) => <h1>{text}</h1>
+
+const Sisalto = ({ kurssi }) => {
+  const osat = kurssi.osat.map(
+    osa => <Osa key={osa.id} nimi={osa.nimi} tehtavia={osa.tehtavia} />)
+
+  return (
+    <div>{osat}</div>
+  )
+}
+
+const Kurssi = ({kurssi}) => {
+
+  return (
+    <div>
+      <Otsikko text={kurssi.nimi} />
+      <Sisalto kurssi={kurssi} />
+    </div>
+  )
+}
+
+const App = ({kurssi}) => {
+
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+
+    </div>
+  )
+}
+
+const kurssi = {
+  nimi: 'Half Stack -sovelluskehitys',
+  osat: [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10,
+      id: 1
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7,
+      id: 2
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14,
+      id: 3
+    },
+    {
+      nimi: 'uusi osa',
+      tehtavia: 1,
+      id: 4
+    }
+  ]
+}
+
+ReactDOM.render(
+  <App kurssi={kurssi}/>,
+  document.getElementById('root')
+)
