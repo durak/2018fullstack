c77a8b1fda7e6d89b5011903cdf71ceee23f0410
durak <itapkos@gmail.com>
2018-05-21 00:44:03 +0300

    osa_02 css ja virheilmoitus

--- osa_02/matsku_02/src/App.js
+++ osa_02/matsku_02/src/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
         this.state = {
             notes: [],
             newNote: 'uusi muistiinpano',
-            showAll: true
+            showAll: true,
+            error: null
         }
         console.log('constructor')
 
@@ -84,14 +85,18 @@ class App extends React.Component {
                 })
             })
             .catch(error => {
-                alert(`muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`)
-                this.setState({ notes: this.state.notes.filter(n => n.id !== id) })
+                this.setState({
+                    error: `muistiinpano '${note.content}' on jo valitettavasti poistettu palvelimelta`,
+                    notes: this.state.notes.filter(n => n.id !== id)
+                })
             })
+        setTimeout(() => {
+            this.setState({ error: null })
+        }, 2000)
 
     }
 
     render() {
-        console.log('render')
         const notesToShow =
             this.state.showAll ?
                 this.state.notes :
@@ -102,6 +107,7 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Muistiinpanot</h1>
+                <Notification message={this.state.error} />
                 <div>
                     <button onClick={this.toggleVisible}>
                         näytä {label}
@@ -120,6 +126,15 @@ class App extends React.Component {
             </div>
         )
     }
+}
+
+const Notification = ({ message }) => {
+    if (message === null) return null
+    return (
+        <div className="error">
+            {message}
+        </div>
+    )
 }
 
 export default App
