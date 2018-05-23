02c013e20e1d2d87a2ac9a4d77d7de69a9df5e33
durak <itapkos@gmail.com>
2018-05-23 14:58:53 +0300

    herokuun

--- osa_03/matsku_03/index.js
+++ osa_03/matsku_03/index.js
@@ -105,7 +105,9 @@ const generateId = () => {
   return maxId + 1
 }
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
  
