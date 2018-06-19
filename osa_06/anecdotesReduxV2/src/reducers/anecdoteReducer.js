f1c0ec9e9f9acfd7109b2dfaa62b79af4a8ad2f0
durak <itapkos@gmail.com>
2018-06-19 22:07:08 +0300

    h6_10 anecdotes init from db

--- osa_06/anecdotesReduxV2/src/reducers/anecdoteReducer.js
+++ osa_06/anecdotesReduxV2/src/reducers/anecdoteReducer.js
@@ -1,11 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,9 +9,8 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (store = initialState, action) => {
+const anecdoteReducer = (store = [], action) => {
 
   switch (action.type) {
     case 'VOTE': {
@@ -30,6 +21,8 @@ const anecdoteReducer = (store = initialState, action) => {
     }
     case 'CREATE':
       return [...store, { content: action.content, id: getId(), votes: 0 }]
+    case 'INIT':
+      return (action.data)
 
     default:
       return store
@@ -47,6 +40,13 @@ export const anecdoteVote = (id) => {
   return {
     type: 'VOTE',
     id: id
+  }
+}
+
+export const anecdoteInitialization = (data) => {
+  return {
+    type: 'INIT',
+    data
   }
 }
 
