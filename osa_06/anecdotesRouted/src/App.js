06a3556a15b2f21584347c3fc795c958aa5f7bc1
durak <itapkos@gmail.com>
2018-06-20 23:48:56 +0300

    h6_18 create history & notification

--- osa_06/anecdotesRouted/src/App.js
+++ osa_06/anecdotesRouted/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Anecdote from './components/Anecdote'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 import { Menu, About, Footer } from './components/common'
 
 
@@ -34,7 +35,16 @@ class App extends React.Component {
 
   addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
-    this.setState({ anecdotes: this.state.anecdotes.concat(anecdote) })
+    this.setState({ 
+      anecdotes: this.state.anecdotes.concat(anecdote),
+      notification: `a new anecdote ${anecdote.content} created!`
+    })
+
+    setTimeout(() => {
+      this.setState({
+        notification: ''
+      })      
+    },10000)
   }
 
   anecdoteById = (id) =>
@@ -60,14 +70,15 @@ class App extends React.Component {
         <Router>
           <div>
             <Menu />
+            <Notification notification={this.state.notification} />
             <Route exact path="/" render={() =>
               <AnecdoteList anecdotes={this.state.anecdotes} />}
             />
             <Route exact path="/anecdotes/:id" render={({ match }) =>
               <Anecdote anecdote={this.anecdoteById(match.params.id)} />}
             />
-            <Route path="/create" render={() =>
-              <AnecdoteForm addNew={this.addNew} />}
+            <Route path="/create" render={({history}) =>
+              <AnecdoteForm history={history} addNew={this.addNew} />}
             />
             <Route path="/about" render={() =>
               <About />}
