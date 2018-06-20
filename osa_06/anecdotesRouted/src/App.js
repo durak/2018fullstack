c6fcaba7c35d34ada1dfd7f0c532b8a4e73047f6
durak <itapkos@gmail.com>
2018-06-20 23:08:27 +0300

    h6_16 routed anecdotes

---
+++ osa_06/anecdotesRouted/src/App.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+import { Menu, About, Footer } from './components/common'
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      anecdotes: [
+        {
+          content: 'If it hurts, do it more often',
+          author: 'Jez Humble',
+          info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+          votes: 0,
+          id: '1'
+        },
+        {
+          content: 'Premature optimization is the root of all evil',
+          author: 'Donald Knuth',
+          info: 'http://wiki.c2.com/?PrematureOptimization',
+          votes: 0,
+          id: '2'
+        }
+      ],
+      notification: ''
+    }
+  }
+
+  addNew = (anecdote) => {
+    anecdote.id = (Math.random() * 10000).toFixed(0)
+    this.setState({ anecdotes: this.state.anecdotes.concat(anecdote) })
+  }
+
+  anecdoteById = (id) =>
+    this.state.anecdotes.find(a => a.id === id)
+
+  vote = (id) => {
+    const anecdote = this.anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    const anecdotes = this.state.anecdotes.map(a => a.id === id ? voted : a)
+
+    this.setState({ anecdotes })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Software anecdotes</h1>
+        <Router>
+          <div>
+            <Menu />
+            <Route exact path="/" render={() =>
+              <AnecdoteList anecdotes={this.state.anecdotes} />}
+            />
+            <Route path="/create" render={() =>
+              <AnecdoteForm addNew={this.addNew} />}
+            />
+            <Route path="/about" render={() =>
+              <About />}
+            />
+                        
+          </div>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ osa_06/anecdotesRouted/src/components/common.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Menu = () => (
+  <div>
+    <Link to="/">anecdotes</Link> &nbsp;    
+    <Link to="/create">create new</Link> &nbsp;
+    <Link to="/about">about</Link> &nbsp;
+  </div >
+)
+
+export const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+    
+    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+      An anecdote is "a story with a point."</em>
+
+    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+  </div>
+)
+
+export const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
+
+    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+  </div>
+)
